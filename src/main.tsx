2d26e698889c390454dd8c6fe79036168d7f0d64
dgdgdgdg <62982921+dg4-design@users.noreply.github.com>
2024-01-24 18:42:32 +0900

    turn into vite

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from '@/App';
+import '@/styles/_reset.css';
+
+import '@/styles/config/size.sass';
+import '@/styles/config/media.sass';
+import '@/styles/config/global.sass';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
