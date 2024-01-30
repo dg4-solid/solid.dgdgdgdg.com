1d9c9d1ca72fd7e6130d22db40ff3f14baa6e8e2
dgdgdgdg <62982921+dg4-design@users.noreply.github.com>
2024-01-30 14:02:04 +0900

    yarnにしたりとかね

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
-import Header from '@/components/header';
+
 import BrandBar from '@/components/brandBar';
+import Header from '@/components/header';
 
 import '@/styles/style.sass';
 
