8cc9c2a4f01b4b98d77ddde01a3b7b4b8eacb58e
dgdgdgdg <ren.y@dgdgdgdg.com>
2025-02-03 14:42:20 +0900

    Update project configuration files

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,11 @@ export default defineConfig({
   site: "https://solid.dgdgdgdg.com",
   integrations: [],
   output: "static",
+  vite: {
+    resolve: {
+      alias: {
+        "@": "/src",
+      },
+    },
+  },
 });
